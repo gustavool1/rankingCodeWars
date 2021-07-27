3277c2561c8db0cfc921c4fa105b5ac5816cc10d
gustavo <go8895806@gmail.com>
2021-07-27 14:35:58 -0300

    api - rafael

--- script.js
+++ script.js
@@ -1,20 +1,43 @@
 
 let nomesPont = [
-{nome:"AldenivanJr" ,pontuacao:194, perfil:"https://www.codewars.com/users/Aldenivan%20Jr"},
-{nome:"debgama", pontuacao: 175, perfil:"https://www.codewars.com/users/deb.gama"},
-{nome:"ericmartins02", pontuacao:490, perfil:"https://www.codewars.com/users/ericmartins02"},
-{nome:"EtnanSousa", pontuacao:218, perfil:"https://www.codewars.com/users/EtnanSousa"},
-{nome:"Fabricio_SantosBR", pontuacao:151, perfil:"https://www.codewars.com/users/Fabricio_SantosBR"},
-{nome:"GuilherminoLucas", pontuacao:681, perfil:"https://www.codewars.com/users/GuilherminoLucas"},
-{nome:"gustavool1", pontuacao:300, perfil:"https://www.codewars.com/users/gustavool1"},
-{nome:"IgorMGG", pontuacao:401, perfil:"https://www.codewars.com/users/IgorMGG"},
-{nome:"larissakoliveira", pontuacao:174, perfil:"https://www.codewars.com/users/larissakoliveira"},
-{nome:"smillyxD", pontuacao:176, perfil:"https://www.codewars.com/users/smillyxD"},
-{nome:"vinapro", pontuacao: 242, perfil:"https://www.codewars.com/users/vinapro"},
-{nome:"midrick", pontuacao: 282, perfil:"https://www.codewars.com/users/midrick"},
-{nome:"Nafly09", pontuacao:477, perfil:"https://www.codewars.com/users/Nafly09"},
-{nome:"marcelochaves92", pontuacao:122, perfil:"https://www.codewars.com/users/marcelochaves92"}
+{nome:"AldenivanJr" ,pontuacao:0, perfil:"https://www.codewars.com/users/Aldenivan%20Jr", api_request: "https://www.codewars.com/api/v1/users/Aldenivan%20Jr" },
+{nome:"debgama", pontuacao: 0, perfil:"https://www.codewars.com/users/deb.gama", api_request: "https://www.codewars.com/api/v1/users/deb.gama"},
+{nome:"ericmartins02", pontuacao:0, perfil:"https://www.codewars.com/users/ericmartins02", api_request: "https://www.codewars.com/api/v1/users/ericmartins02"},
+{nome:"EtnanSousa", pontuacao:0, perfil:"https://www.codewars.com/users/EtnanSousa", api_request: "https://www.codewars.com/api/v1/users/EtnanSousa"},
+{nome:"Fabricio_SantosBR", pontuacao:0, perfil:"https://www.codewars.com/users/Fabricio_SantosBR", api_request: "https://www.codewars.com/api/v1/users/Fabricio_SantosBR"},
+{nome:"GuilherminoLucas", pontuacao:0, perfil:"https://www.codewars.com/users/GuilherminoLucas", api_request: "https://www.codewars.com/api/v1/users/GuilherminoLucas"},
+{nome:"gustavool1", pontuacao:0, perfil:"https://www.codewars.com/users/gustavool1", api_request: "https://www.codewars.com/api/v1/users/gustavool1"},
+{nome:"IgorMGG", pontuacao:0, perfil:"https://www.codewars.com/users/IgorMGG", api_request: "https://www.codewars.com/api/v1/users/IgorMGG"},
+{nome:"larissakoliveira", pontuacao:0, perfil:"https://www.codewars.com/users/larissakoliveira", api_request: "https://www.codewars.com/api/v1/users/larissakoliveira"}, 
+{nome:"smillyxD", pontuacao:0, perfil:"https://www.codewars.com/users/smillyxD", api_request: "https://www.codewars.com/api/v1/users/smillyxD"},
+{nome:"vinapro", pontuacao: 0, perfil:"https://www.codewars.com/users/vinapro", api_request: "https://www.codewars.com/api/v1/users/vinapro"},
+{nome:"midrick", pontuacao: 0, perfil:"https://www.codewars.com/users/midrick", api_request: "https://www.codewars.com/api/v1/users/midrick"},
+{nome:"Nafly09", pontuacao:0, perfil:"https://www.codewars.com/users/Nafly09", api_request: "https://www.codewars.com/api/v1/users/Nafly09"},
+{nome:"marcelochaves92", pontuacao:0, perfil:"https://www.codewars.com/users/marcelochaves92", api_request: "https://www.codewars.com/api/v1/users/marcelochaves92"},
+{nome:"Rafael", pontuacao:0, perfil:"https://www.codewars.com/users/rafa.coding.true", api_request: "https://www.codewars.com/api/v1/users/rafa.coding.true"}
 ]
+
+// OBTER RANK ATUAL E ATUALIZAR O VALOR NO OBJETO
+
+function httpGet(url){
+
+    let xmlHttp = new XMLHttpRequest();
+    xmlHttp.open( "GET", url, false ); 
+    xmlHttp.send( null );
+    
+    let result = xmlHttp.responseText.split(',')[2];
+    result = result.substr(result.indexOf(':')+1, result.length);
+
+    return Number(result);
+}
+
+for(let item in nomesPont){
+    console.log(nomesPont[item].nome, httpGet(nomesPont[item].api_request));
+    nomesPont[item].pontuacao = httpGet(nomesPont[item].api_request);
+}
+
+// =================================================================
+
 nomesPont.sort(function(a,b){
     return b.pontuacao - a.pontuacao
 })
@@ -40,9 +63,6 @@ function creatingTable(){
 let checkContent = document.createElement('section')
 
 let p = document.createElement("p")
-let p1 = document.createElement("p")
-p1.innerHTML = '<br>Clique no nick e irá ser redirecionado para o perfil'
-p.innerHTML='Atualizado por API- Feito pelo Rafael Schug <i class="far fa-clock"></i>'
+p.innerHTML='Atualizado por Api pelo Rafael Schug <i class="far fa-clock"></i>'
 checkContent.appendChild(p)
-checkContent.appendChild(p1)
 document.body.appendChild(checkContent)
