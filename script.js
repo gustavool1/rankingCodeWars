51779c28fb0c3af264ed07c208e3da66c243c618
gustavo <go8895806@gmail.com>
2021-07-27 08:25:02 -0300

    links nos tr

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
 
 let nomesPont = [
-{nome:"AldenivanJr" ,pontuacao:174},
-{nome:"debgama", pontuacao: 165},
-{nome:"ericmartins02", pontuacao:455},
-{nome:"EtnanSousa", pontuacao:204},
-{nome:"Fabricio_SantosBR", pontuacao:133},
-{nome:"GuilherminoLucas", pontuacao:539},
-{nome:"gustavool1", pontuacao:296},
-{nome:"IgorMGG", pontuacao:361},
-{nome:"larissakoliveira", pontuacao:166},
-{nome:"smillyxD", pontuacao:159},
-{nome:"vinapro", pontuacao: 229},
-{nome:"midrick", pontuacao: 259},
-{nome:"Nafly09", pontuacao:428}
+{nome:"AldenivanJr" ,pontuacao:174, perfil:"https://www.codewars.com/users/Aldenivan%20Jr"},
+{nome:"debgama", pontuacao: 165, perfil:"https://www.codewars.com/users/deb.gama"},
+{nome:"ericmartins02", pontuacao:455, perfil:"https://www.codewars.com/users/ericmartins02"},
+{nome:"EtnanSousa", pontuacao:204, perfil:"https://www.codewars.com/users/EtnanSousa"},
+{nome:"Fabricio_SantosBR", pontuacao:133, perfil:"https://www.codewars.com/users/Fabricio_SantosBR"},
+{nome:"GuilherminoLucas", pontuacao:539, perfil:"https://www.codewars.com/users/GuilherminoLucas"},
+{nome:"gustavool1", pontuacao:296, perfil:"https://www.codewars.com/users/gustavool1"},
+{nome:"IgorMGG", pontuacao:361, perfil:"https://www.codewars.com/users/IgorMGG"},
+{nome:"larissakoliveira", pontuacao:166, perfil:"https://www.codewars.com/users/larissakoliveira"},
+{nome:"smillyxD", pontuacao:159, perfil:"https://www.codewars.com/users/smillyxD"},
+{nome:"vinapro", pontuacao: 229, perfil:"https://www.codewars.com/users/vinapro"},
+{nome:"midrick", pontuacao: 259, perfil:"https://www.codewars.com/users/midrick"},
+{nome:"Nafly09", pontuacao:428, perfil:"https://www.codewars.com/users/Nafly09"}
 ]
 nomesPont.sort(function(a,b){
     return b.pontuacao - a.pontuacao
@@ -31,8 +31,9 @@ function creatingTable(){
     main.append(tableRanking)
     for(let i=0;i<5;i++){
         let celula = document.createElement("tr")
-        celula.innerHTML = `<td>${nomesPont[i].nome}</td><td>${nomesPont[i].pontuacao}</td>`
+        celula.innerHTML = `<td><a href='${nomesPont[i].perfil}' target=_blank>${nomesPont[i].nome}</a></td><td>${nomesPont[i].pontuacao}</td>`
         tableBody.appendChild(celula)
+        
     } 
 }
 let checkContent = document.createElement('section')
