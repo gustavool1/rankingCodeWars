310d66201c5f51dfea077d4c3ae12ff9a8bc59ea
gustavo <go8895806@gmail.com>
2021-07-30 21:06:44 -0300

    helper.js

---
+++ helper.js
@@ -0,0 +1,19 @@
+const nomesPont = [
+    {nome:"AldenivanJr" ,pontuacao:0, perfil:"https://www.codewars.com/users/Aldenivan%20Jr", api_request: "https://www.codewars.com/api/v1/users/Aldenivan%20Jr" },
+    {nome:"debgama", pontuacao: 0, perfil:"https://www.codewars.com/users/deb.gama", api_request: "https://www.codewars.com/api/v1/users/deb.gama"},
+    {nome:"ericmartins02", pontuacao:0, perfil:"https://www.codewars.com/users/ericmartins02", api_request: "https://www.codewars.com/api/v1/users/ericmartins02"},
+    {nome:"EtnanSousa", pontuacao:0, perfil:"https://www.codewars.com/users/EtnanSousa", api_request: "https://www.codewars.com/api/v1/users/EtnanSousa"},
+    {nome:"Fabricio_SantosBR", pontuacao:0, perfil:"https://www.codewars.com/users/Fabricio_SantosBR", api_request: "https://www.codewars.com/api/v1/users/Fabricio_SantosBR"},
+    {nome:"GuilherminoLucas", pontuacao:0, perfil:"https://www.codewars.com/users/GuilherminoLucas", api_request: "https://www.codewars.com/api/v1/users/GuilherminoLucas"},
+    {nome:"gustavool1", pontuacao:0, perfil:"https://www.codewars.com/users/gustavool1", api_request: "https://www.codewars.com/api/v1/users/gustavool1"},
+    {nome:"IgorMGG", pontuacao:0, perfil:"https://www.codewars.com/users/IgorMGG", api_request: "https://www.codewars.com/api/v1/users/IgorMGG"},
+    {nome:"larissakoliveira", pontuacao:0, perfil:"https://www.codewars.com/users/larissakoliveira", api_request: "https://www.codewars.com/api/v1/users/larissakoliveira"}, 
+    {nome:"smillyxD", pontuacao:0, perfil:"https://www.codewars.com/users/smillyxD", api_request: "https://www.codewars.com/api/v1/users/smillyxD"},
+    {nome:"vinapro", pontuacao: 0, perfil:"https://www.codewars.com/users/vinapro", api_request: "https://www.codewars.com/api/v1/users/vinapro"},
+    {nome:"vini144", pontuacao: 0, perfil:"https://www.codewars.com/users/vini144", api_request: "https://www.codewars.com/api/v1/users/vini144"},
+    {nome:"Nafly09", pontuacao:0, perfil:"https://www.codewars.com/users/Nafly09", api_request: "https://www.codewars.com/api/v1/users/Nafly09"},
+    {nome:"marcelochaves92", pontuacao:0, perfil:"https://www.codewars.com/users/marcelochaves92", api_request: "https://www.codewars.com/api/v1/users/marcelochaves92"},
+    {nome:"RafaelSchug", pontuacao:0, perfil:"https://www.codewars.com/users/rafa.coding.true", api_request: "https://www.codewars.com/api/v1/users/rafa.coding.true"},
+    {nome:"Ângelo Vieira", pontuacao:0, perfil:"https://www.codewars.com/users/%C3%82ngelo%20Vieira", api_request:"https://www.codewars.com/api/v1/users/Ângelo%20Vieira"},
+    {nome:"zvgabrielvz", pontuacao:0, perfil:"https://www.codewars.com/users/zvgabrielvz", api_request:"https://www.codewars.com/api/v1/users/zvgabrielvz"}   
+]
